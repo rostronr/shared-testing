2bf838cb0d11fc25699bf81635f3e6d7b05e1212
riofriz <comm.campione@gmail.com>
2017-07-28 23:37:35 +0100

    php requires ssl certificate. ffs.

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <h1>YA 'KNOW WHAT I'M SAYIN'????</h1>
 
 <ul>
-	<li><a href="/shared-testing/augmented-1/index.php">augmented-1</a></li>
+	<li><a href="/shared-testing/augmented-1/index.html">augmented-1</a></li>
 </ul>
