ceb41d4a8d994f56588dbc9660c02c9b60d99d69
riofriz <comm.campione@gmail.com>
2017-07-28 23:30:37 +0100

    i am being an idiot

--- augmented-1/index.php
+++ augmented-1/index.php
@@ -1,18 +1,18 @@
 <!-- AR.js by @jerome_etienne - github: https://github.com/jeromeetienne/ar.js - info: https://medium.com/arjs/augmented-reality-in-10-lines-of-html-4e193ea9fdbf -->
 <script src="https://aframe.io/releases/0.6.0/aframe.min.js"></script>
-<script src="/shared-testing/js/aframe-ar.js"></script>
+<script src="/shared-testing/augmented-1/js/aframe-ar.js"></script>
 <body style='margin : 0px; overflow: hidden;'>
 	<a-scene embedded arjs='sourceType: webcam;'>
 		<a-assets>
-			<a-asset-item id="spinner-obj" src="/shared-testing/model/spinner.obj"></a-asset-item>
-			<a-asset-item id="spinner-mtl" src="/shared-testing/model/spinner.mtl"></a-asset-item>
+			<a-asset-item id="spinner-obj" src="/shared-testing/augmented-1/model/spinner.obj"></a-asset-item>
+			<a-asset-item id="spinner-mtl" src="/shared-testing/augmented-1/model/spinner.mtl"></a-asset-item>
 		</a-assets>
 
 		<!-- Using the asset management system. -->
 		<a-obj-model src="#spinner-obj" mtl="#spinner-mtl"></a-obj-model>
 
 		<!-- Defining the URL inline. Not recommended but may be more comfortable. -->
-		<a-obj-model src="/shared-testing/model/spinner.obj" mtl="/shared-testing/model/spinner.mtl"></a-obj-model>
+		<a-obj-model src="/shared-testing/augmented-1/model/spinner.obj" mtl="/shared-testing/augmented-1/model/spinner.mtl"></a-obj-model>
 
 		<a-marker-camera preset='hiro'></a-marker-camera>
 	</a-scene>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <h1>YA 'KNOW WHAT I'M SAYIN'????</h1>
 
 <ul>
-	<li><a href="/augmented-1/index.php">augmented-1</a></li>
+	<li><a href="/shared-testing/augmented-1/index.php">augmented-1</a></li>
 </ul>
